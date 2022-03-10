fffb988dec439f528bf2a7034fe43000bb8c7753
bhagirathsinh29 <bhagirathsinh@formics.io>
2022-03-10 11:03:12 +0530

    call.dart update

--- lib/src/pages/call.dart
+++ lib/src/pages/call.dart
@@ -337,7 +337,7 @@ class _CallPageState extends State<CallPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Agora Flutter QuickStart'),
+        title: Text('Video call clone'),
       ),
       backgroundColor: Colors.black,
       body: Center(
