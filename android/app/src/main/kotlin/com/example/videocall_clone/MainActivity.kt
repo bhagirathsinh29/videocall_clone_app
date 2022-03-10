b291b2d19292592784a1565a7111077c6da5a6ef
bhagirathsinh29 <bhagirathsinh@formics.io>
2022-03-10 10:08:54 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/videocall_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.videocall_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,5 @@
+/// Define App ID and Token
+const APP_ID = "4aba5fcb94604a97b057100a05d48d45";
+const Token =
+    "0064aba5fcb94604a97b057100a05d48d45IACGAZE1Cy0WCxo7+UTzfXT1RDRKAVuNbcERIrnmjdRERhXUZI4AAAAAEABgTjz6S98pYgEAAQBK3yli";
+const Channelname = "demoapp";
