b2474cc20a5ae5219c9daac488170a476aa3365d
bhagirathsinh29 <bhagirathsinh@formics.io>
2022-03-12 11:32:10 +0530

    token change

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,5 +1,5 @@
 /// Define App ID and Token
 const APP_ID = "4aba5fcb94604a97b057100a05d48d45";
 const Token =
-    "0064aba5fcb94604a97b057100a05d48d45IACGAZE1Cy0WCxo7+UTzfXT1RDRKAVuNbcERIrnmjdRERhXUZI4AAAAAEABgTjz6S98pYgEAAQBK3yli";
+    "0064aba5fcb94604a97b057100a05d48d45IACqyTZkgAqEGDjlqhKmABy+8KLCBKhllRsEecWDSKM68RXUZI4AAAAAEAAb78m5vYgtYgEAAQC9iC1i";
 const Channelname = "demoapp";
